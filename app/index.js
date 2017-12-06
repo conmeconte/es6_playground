ee9b042e909e2bf8d23627c690ea7af269dd7b0d
conmeconte <saja523@hotmail.com>
2017-12-06 12:55:13 -0800

    starting new branch

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
-console.log('App working: open app/index.js to start exploring ES6');
+
+//if you want to import everything
+import * as math from './calc'
+console.log('math:', math);
+console.log('5 + 7 = ', math.default(5, 7));
+
+console.log('8 - 9 = ', math.subtract(8, 9));
+
+console.log('9 x 10 = ', math.multiply(9, 10));
+
+// if you want to import ont by one
+// import addStuff, {subtract, multiply} from './calc';
+//
+//
+//
+// console.log('5 + 7 = ', addStuff(5, 7));
+//
+// console.log('8 - 9 = ', subtract(8, 9));
+//
+// console.log('9 x 10 = ', multiply(9, 10));
